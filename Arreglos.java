bf0c21d1871f01104e935ee5a61f7fdfbed9b926
aylinmf <56418413+aylinmf@users.noreply.github.com>
2019-11-12 15:52:53 -0600

    Add files via upload

---
+++ Arreglos.java
@@ -0,0 +1,42 @@
+
+package arreglos;
+
+
+public class Arreglos {
+
+    
+    public static void main(String[] args) {
+        
+      String[] nombre = new String[10];
+      
+      nombre[0] = "Katerin Guifarro";
+      nombre[1] = "Job Ramos";
+      nombre[2] = "Jesus Acosta";
+      nombre[3] = "Kevin Archaga";
+      nombre[4] = "Cristian Menjivar";
+      nombre[5] = "Keissy Perdomo";
+      nombre[6] = "Jeffree Reyes";
+      nombre[7] = "Ingris Reyes";
+      nombre[8] = "Scarlett Castillo";
+      nombre[9] = "Ivan Torres";
+      
+     
+     System.out.println("*Arreglo que imprime 10 nombres de compañeros de clase.*"+ "\n");
+      
+     System.out.println("1." +nombre[0]);
+     System.out.println("2." +nombre[1]);
+     System.out.println("3." +nombre[2]);
+     System.out.println("4." +nombre[3]);
+     System.out.println("5." +nombre[4]); 
+     System.out.println("6." +nombre[5]);
+     System.out.println("7." +nombre[6]);
+     System.out.println("8." +nombre[7]);
+     System.out.println("9." +nombre[8]);
+     System.out.println("10."+nombre[9]);
+     
+     
+     System.out.println("\n");
+        
+    }
+    
+}
